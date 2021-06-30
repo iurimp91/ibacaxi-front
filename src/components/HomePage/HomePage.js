8cb631b089ec91225258691d1b433806d2beba20
Iuri Magnago <iurimp91@gmail.com>
2021-06-30 10:17:18 -0300

    Get products filtering by categories or sorting out by price

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -5,43 +5,113 @@ import ProductsShowcase from "../ProductsShowcase/ProductsShowcase";
 
 export default function HomePage() {
     const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState([]);
+    let categoryQuery = "";
+    let priceQuery = "";
 
     useEffect(() => {
-        const prodRequest = axios.get("http://localhost:4000/products");
+       getProducts();
+    }, []);
+
+    function getProducts(categoryQuery, priceQuery) {
+        const prodRequest = axios.get(`http://localhost:4000/products?${categoryQuery}`);
         prodRequest.then((respose) => {
             setProducts(respose.data);
         });
         prodRequest.catch((error) => alert("error"));
-    }, []);
+    }
 
+    useEffect(() => {
+        if(categories.length !== 0) {
+            return;
+        }
+        let previousCategory = [];
+        const existingCategories = products.filter(item => {
+            if(previousCategory.includes(item.categoryName)) {
+                return false;
+            } else {
+                previousCategory.push(item.categoryName);
+                return true;
+            }
+        });
+    
+        const categoriesNames = existingCategories.map(item => {
+            return {categoryName: item.categoryName, categoryId: item.categoryId};
+        });
+
+        setCategories(categoriesNames.sort((a,b) => {
+            return (a.categoryName > b.categoryName) ? 1 : ((b.categoryName > a.categoryName) ? -1 : 0);
+        }));
+    }, [products]);
+
+    function makeCategoryQuery(categoryId) {
+        categoryQuery = `category=${categoryId}&`;
+        getProducts(categoryQuery);
+    }
+
+    function makePriceQuery(price) {
+        priceQuery = `price=${price}&`;
+        getProducts(priceQuery);
+    }
+    
     return (
         <Home>
             <h1>Essa é a linda página principal.</h1>
             <Container>
                 <Sidebar>
-                    <ul>
-                        <li>Placeholder</li>
-                        <li>Placeholder</li>
-                        <li>Placeholder</li>
-                    </ul>
+                    <CategoriesList>
+                        <h1>Filter by</h1>
+                        {categories.map((item, i) => 
+                            <li onClick={() => makeCategoryQuery(item.categoryId)}>{item.categoryName}</li>
+                        )}
+                    </CategoriesList>
+                    <PriceFilter>
+                        <h1>Order by</h1>
+                        <li onClick={() => makePriceQuery("")}>Standard</li>
+                        <li onClick={() => makePriceQuery("higher")}>Higher price</li>
+                        <li onClick={() => makePriceQuery("lower")}>Lower price</li>
+                    </PriceFilter>       
                 </Sidebar>
-                <ProductsShowcase products={products} />
+                <div className="products-list">
+                    <ProductsShowcase products={products} />
+                </div>
             </Container>
         </Home>
     );
 }
+
 const Home = styled.div`
     background-color: #e1e5ea;
     height: 100%;
 `;
+
 const Container = styled.div`
     display: flex;
     height: 100%;
+
+    .products-list {
+        margin-left: 100px;
+    }
 `;
+
 const Sidebar = styled.div`
     display: flex;
     flex-direction: column;
     width: 100px;
     background-color: blue;
-    height: 100%;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    margin-top: 26px;
+`;
+
+const CategoriesList = styled.ul`
+    display: flex;
+    flex-direction: column;
+`;
+
+const PriceFilter = styled.ul`
+    display: flex;
+    flex-direction: column;
 `;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -3,7 +3,6 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
     body {
         font-family: "Roboto";
-        height: 100vh;
     }
     #root {
         background-color: black;
